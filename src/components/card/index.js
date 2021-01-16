bdbd7e226b1eb85f0537ea25e4c8585e3255b58d
Werawit <werawit_boat@windowslve.com>
2021-01-16 13:57:22 +0700

    Add: basic setup react app

---
+++ src/components/card/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { CardContainer } from "./styles";
+
+const Card = () => {
+    return (
+        <CardContainer>
+            <h1>Hello from Card.</h1>
+        </CardContainer>
+    );
+};
+
+export default Card;
